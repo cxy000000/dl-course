7a390e4cb6d680c8fb4a0d92abaec0a22fb032e7
cxy000000 <chenxyxy06@gmail.com>
2023-10-03 23:06:03 +0800

    使用pyqt

--- exp1/main.py
+++ exp1/main.py
@@ -1,9 +1,10 @@
-from utils import WalkingRouteGUI
-import tkinter as tk
-    
+from utils import RouteGUI
+import sys
+from PyQt5.QtWidgets import QApplication
+
 if __name__ == "__main__":
-    api_key = '1c5a6f863e2b10154c7ec8357f11f740' 
-    
-    root = tk.Tk()
-    app = WalkingRouteGUI(root, api_key)
-    root.mainloop()
+    api_key = '1c5a6f863e2b10154c7ec8357f11f740'
+    app = QApplication(sys.argv)
+    window = RouteGUI(api_key)
+    window.show()
+    sys.exit(app.exec_())
--- exp1/utils.py
+++ exp1/utils.py
@@ -1,21 +1,20 @@
+from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QPushButton, QTextEdit
+from PyQt5.QtCore import Qt
 import requests
-import tkinter as tk
-from tkinter import messagebox
-from ttkbootstrap import Style
+from uiwindow import Ui_MainWindow 
 
-
-class WalkingRoute:
+class Route:
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_route(self, origin, destination):
+    def get_walkingroute(self, origin, destination):
         url = f'https://restapi.amap.com/v3/direction/walking?origin={origin}&destination={destination}&key={self.api_key}'
         try:
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()
                 if data['status'] == '1':
-                    routes = list(route for route in data['route']['paths'])
+                    routes = data['route']['paths']
                     return routes
                 else:
                     print('未找到路径')
@@ -37,7 +36,6 @@ class WalkingRoute:
                     geocodes = data['geocodes']
                     if geocodes:
                         location = geocodes[0]['location']  # 坐标点（经度，纬度）
-                        # print(f'坐标点: {location}')
                         return location
                     else:
                         print('未找到匹配的地址')
@@ -48,63 +46,31 @@ class WalkingRoute:
         except Exception as e:
             print(f'发生异常: {str(e)}')
 
+class RouteGUI(QMainWindow):
+    def __init__(self, api_key):
+        super().__init__()
+        self.route = Route(api_key)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.get_walking_route)
 
-class WalkingRouteGUI(WalkingRoute):
-    def __init__(self, root, api_key):
-        super().__init__(api_key)
-        style = Style()
-        style = Style(theme='sandstone')
-        #主题可替换为：['vista', 'classic', 'cyborg', 'journal', 'darkly', 'flatly', 'clam', 'alt', 'solar', 'minty', 'litera', 'united', 'xpnative', 'pulse', 'cosmo', 'lumen', 'yeti', 'superhero', 'winnative', 'sandstone', 'default']
-        TOP6 = style.master
-        self.root = root
-        self.root.title("Walking Route")
-
-        # 出发点城市和地址输入框
-        self.ori_city_label = tk.Label(root, text="出发点城市:")
-        self.ori_city_label.pack()
-        self.ori_city_entry = tk.Entry(root)
-        self.ori_city_entry.pack()
-        self.ori_address_label = tk.Label(root, text="出发点地址:")
-        self.ori_address_label.pack()
-        self.ori_address_entry = tk.Entry(root)
-        self.ori_address_entry.pack()
-
-        # 目的地城市和地址输入框
-        self.des_city_label = tk.Label(root, text="目的地城市:")
-        self.des_city_label.pack()
-        self.des_city_entry = tk.Entry(root)
-        self.des_city_entry.pack()
-        self.des_address_label = tk.Label(root, text="目的地地址:")
-        self.des_address_label.pack()
-        self.des_address_entry = tk.Entry(root)
-        self.des_address_entry.pack()
-
-        # 步行路径规划按钮
-        self.route_button = tk.Button(root, text="步行路径规划", command=self.get_walking_route)
-        self.route_button.pack()
-        
-        # 结果显示文本框
-        self.result_text = tk.Text(root, state=tk.DISABLED, height=30, width=40)
-        self.result_text.pack()
-        
     def display_message(self, message):
-        self.result_text.config(state=tk.NORMAL)
-        self.result_text.insert(tk.END, message + '\n')
-        self.result_text.config(state=tk.DISABLED)
+        self.ui.textEdit.setPlainText(self.ui.textEdit.toPlainText() + message + '\n')
 
     def get_walking_route(self):
-        ori_city = self.ori_city_entry.get()
-        ori_address = self.ori_address_entry.get()
-        des_city = self.des_city_entry.get()
-        des_address = self.des_address_entry.get()
+        ori_city = self.ui.lineEdit.text()
+        ori_address = self.ui.lineEdit_2.text()
+        des_city = self.ui.lineEdit_3.text()
+        des_address = self.ui.lineEdit_4.text()
 
-        ori_geocode = self.get_geocode(ori_address, ori_city)
+        ori_geocode = self.route.get_geocode(ori_address, ori_city)
         self.display_message(f'出发点坐标: {ori_geocode}')
-        des_geocode = self.get_geocode(des_address, des_city)
+        des_geocode = self.route.get_geocode(des_address, des_city)
         self.display_message(f'目的地坐标: {des_geocode}')
 
         if ori_geocode and des_geocode:
-            routes = self.get_route(ori_geocode, des_geocode)
+            routes = self.route.get_walkingroute(ori_geocode, des_geocode)
+            self.ui.textEdit.clear()
             for route in routes:
                 distance = route['distance']  # 步行距离（米）
                 duration = route['duration']  # 步行时间（秒）
@@ -114,4 +80,3 @@ class WalkingRouteGUI(WalkingRoute):
                 for step in route['steps']:
                     instruction = step['instruction']
                     self.display_message(f'- {instruction}')
-
