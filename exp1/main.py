4cd57e5fbcf7b1111ec9296e7f3f4be035e3e6b3
cxy000000 <chenxyxy06@gmail.com>
2023-10-08 22:05:11 +0800

    添加静态地图功能，添加页面跳联

--- exp1/main.py
+++ exp1/main.py
@@ -1,10 +1,27 @@
-from utils import RouteGUI
+from utils import RouteGUI, MenuGUI, SMapGUI
 import sys
 from PyQt5.QtWidgets import QApplication
+
+def closeAndShowRouteWindow(w_1, w_2):
+    w_1.close()
+    w_2.show()
 
 if __name__ == "__main__":
     api_key = '1c5a6f863e2b10154c7ec8357f11f740'
     app = QApplication(sys.argv)
-    window = RouteGUI(api_key)
-    window.show()
-    sys.exit(app.exec_())
+    w_menu = MenuGUI()
+    w_route = RouteGUI(api_key)
+    w_map = SMapGUI(api_key)
+    w_menu.show()
+    
+    # 跳联关系
+    w_menu.ui.pushButton_r.clicked.connect(lambda: closeAndShowRouteWindow(w_menu, w_route))
+    w_menu.ui.pushButton_m.clicked.connect(lambda: closeAndShowRouteWindow(w_menu, w_map))
+    w_route.ui.pushButton_back.clicked.connect(lambda: closeAndShowRouteWindow(w_route, w_menu))
+    w_map.ui.pushButton_back.clicked.connect(lambda: closeAndShowRouteWindow(w_map, w_menu))
+
+    w_menu.ui.pushButton_quit.clicked.connect(app.quit)
+    app.exec_()
+    
+    
+
--- exp1/utils.py
+++ exp1/utils.py
@@ -1,9 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QLineEdit, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import Qt
 import requests
-from uiwindow import Ui_MainWindow 
+from uiwindow import Ui_MainWindow as Ui_MainWindow_route
+from menu import Ui_MainWindow as Ui_MainWindow_menu
+from uiwindow2 import Ui_MainWindow as Ui_MainWindow_smap
 from PyQt5.QtGui import QPixmap, QPalette, QBrush, QPainter
-class Route:
+class Map_API:
     def __init__(self, api_key):
         self.api_key = api_key
 
@@ -73,12 +75,26 @@ class Route:
                 print('请求失败')
         except Exception as e:
             print(f'发生异常: {str(e)}')
+            
+    def get_static_map(self, location, zoom, size):
+        # url = f'https://restapi.amap.com/v3/staticmap?location=116.481485,39.990464&zoom=10&size=750*300&markers=mid,,A:116.481485,39.990464&key={self.api_key}'
+        url = f'https://restapi.amap.com/v3/staticmap?&location={location}&zoom={zoom}&size={size}&markers=mid,0x008000,A:{location}&key={self.api_key}&output=JSON'
+        # # https://restapi.amap.com/v3/staticmap?markers=mid,0xFF0000,A:116.37359,39.92437;116.47359,39.92437&key=您的key
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                return response.content
+            else:
+                print('请求失败')
+        except Exception as e:
+            print(f'发生异常: {str(e)}')
+            
 
 class RouteGUI(QMainWindow):
     def __init__(self, api_key):
         super().__init__()
-        self.route = Route(api_key)
-        self.ui = Ui_MainWindow()
+        self.route = Map_API(api_key)
+        self.ui = Ui_MainWindow_route()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.get_walking_route)
         self.ui.pushButton_2.clicked.connect(self.get_riding_route)
@@ -108,6 +124,7 @@ class RouteGUI(QMainWindow):
         self.ui.pushButton.setStyleSheet(button_style)
         self.ui.pushButton_2.setStyleSheet(button_style)
         self.ui.pushButton_3.setStyleSheet(button_style)
+        self.ui.pushButton_back.setStyleSheet(button_style)
 
         # 设置窗口背景样式
         background_image = QPixmap("background.png")
@@ -124,9 +141,6 @@ class RouteGUI(QMainWindow):
         palette = QPalette()
         palette.setBrush(QPalette.Window, QBrush(background_image2)) 
         self.setPalette(palette)
-        # window_style = "background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, " \
-        #                "stop:0 rgba(255,255,255, 200), stop:1 rgba(20,196,188, 210));"
-        # self.setStyleSheet(window_style)
 
         # 设置layout的样式
         frame_style = "background-color: rgb(255, 255, 255);" \
@@ -217,3 +231,123 @@ class RouteGUI(QMainWindow):
                 for step in route['steps']:
                     instruction = step['instruction']
                     self.display_message(f'- {instruction}')
+                    
+                    
+class MenuGUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow_menu()
+        self.ui.setupUi(self)
+        self.setup_ui_style()
+
+    # 美化界面
+    def setup_ui_style(self):
+        # 设置按钮样式,
+        button_style = "QPushButton{font: 25 13pt '微软雅黑 Light';color: rgb(255,255,255);" \
+                       "background-color: rgb(20,196,188);" \
+                       "border: none;border-radius:15px;}" \
+                       "QPushButton:hover{background-color: rgb(22,218,208);}" \
+                       "QPushButton:pressed{background-color: rgb(17,171,164);}"
+
+        self.ui.pushButton_r.setStyleSheet(button_style)
+        self.ui.pushButton_m.setStyleSheet(button_style)
+        self.ui.pushButton_quit.setStyleSheet(button_style)
+
+        # 设置窗口背景样式
+        background_image = QPixmap("background.png")
+        background_image = background_image.scaled(850, 850)
+        
+        # 改变透明度
+        background_image2 = QPixmap(background_image.size())
+        background_image2.fill(Qt.transparent) 
+        painter = QPainter(background_image2)
+        painter.setOpacity(0.6) 
+        painter.drawPixmap(0, 0, background_image)
+        painter.end()
+
+        palette = QPalette()
+        palette.setBrush(QPalette.Window, QBrush(background_image2)) 
+        self.setPalette(palette)
+
+        # 设置layout的样式
+        frame_style = "background-color: rgb(255, 255, 255);" \
+                      "border: 2px solid rgb(255,255,255);border-radius: 15px;"
+        self.ui.verticalLayoutWidget.setStyleSheet(frame_style)
+
+        # 设置lineEdit的样式
+        line_style = "color: rgb(31,31,31);" \
+                     "background-color: rgb(255, 255, 255);" \
+                    #  "border:2px solid rgb(20,196,188);border-radius:15px;"
+        self.ui.lineEdit.setStyleSheet(line_style)
+
+        
+class SMapGUI(QMainWindow):
+    def __init__(self, api_key):
+        super().__init__()
+        self.route = Map_API(api_key)
+        self.ui = Ui_MainWindow_smap()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.get_image)
+        self.setup_ui_style()
+
+    # 美化界面
+    def setup_ui_style(self):
+        # 设置标签样式
+        label_style = "font: 25 11pt '微软雅黑 Light';" \
+                      "color: rgb(31,31,31);" \
+                      "background-color: rgb(255, 255, 255);" \
+                      "border:2px solid rgb(255,255,255);border-radius:15px;"
+
+        self.ui.label.setStyleSheet(label_style)
+        self.ui.label_2.setStyleSheet(label_style)
+
+        # 设置按钮样式,
+        button_style = "QPushButton{font: 25 14pt '微软雅黑 Light';color: rgb(255,255,255);" \
+                       "background-color: rgb(20,196,188);" \
+                       "border: none;border-radius:15px;}" \
+                       "QPushButton:hover{background-color: rgb(22,218,208);}" \
+                       "QPushButton:pressed{background-color: rgb(17,171,164);}"
+
+        self.ui.pushButton_back.setStyleSheet(button_style)
+        self.ui.pushButton.setStyleSheet(button_style)
+
+        # 设置窗口背景样式
+        background_image = QPixmap("background.png")
+        background_image = background_image.scaled(850, 850)
+        
+        # 改变透明度
+        background_image2 = QPixmap(background_image.size())
+        background_image2.fill(Qt.transparent) 
+        painter = QPainter(background_image2)
+        painter.setOpacity(0.6) 
+        painter.drawPixmap(0, 0, background_image)
+        painter.end()
+
+        palette = QPalette()
+        palette.setBrush(QPalette.Window, QBrush(background_image2)) 
+        self.setPalette(palette)
+
+        # 设置layout的样式
+        frame_style = "background-color: rgb(255, 255, 255);" \
+                      "border: 2px solid rgb(200,200,200);border-radius: 15px;"
+        self.ui.gridLayoutWidget.setStyleSheet(frame_style)
+
+        # 设置lineEdit的样式
+        line_style = "font: 25 11pt '微软雅黑 Light';" \
+                     "color: rgb(31,31,31);" \
+                     "background-color: rgb(255, 255, 255);" \
+                     "border:2px solid rgb(20,196,188);border-radius:15px;"
+        self.ui.lineEdit.setStyleSheet(line_style)
+
+    def get_image(self):
+        location = self.ui.lineEdit.text()
+        zoom = self.ui.spinBox.text()
+        loc_geocode = self.route.get_geocode(location)
+        size = '750*550'
+
+        if loc_geocode:
+            map_image = self.route.get_static_map(loc_geocode, zoom, size)
+            pixmap = QPixmap()
+            pixmap.loadFromData(map_image)
+            self.ui.label_3.setPixmap(pixmap)
+                    
